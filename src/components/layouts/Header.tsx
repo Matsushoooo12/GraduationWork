78f4a995baae512b17200350a57765f71a6582c9
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-05-24 10:41:59 +0900

    Header作成

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,117 @@
+import {
+  Avatar,
+  Box,
+  Drawer,
+  DrawerContent,
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IconAdjustmentsHorizontal } from "@tabler/icons";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Header: React.FC<Props> = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Flex>
+        <Box
+          w="240px"
+          minHeight="100vh"
+          maxHeight="100%"
+          bg="blue.900"
+          boxShadow="md"
+          display={{ base: "none", lg: "block" }}
+        ></Box>
+        <Box flex="1" bg="white">
+          <Box
+            w="100%"
+            h={{ base: "64px", md: "64px", lg: "80px" }}
+            boxShadow="md"
+            px={8}
+            position="fixed"
+            backgroundColor="white"
+          >
+            <Flex h="100%" alignItems="center">
+              <Icon
+                as={GiHamburgerMenu}
+                display={{ base: "block", md: "block", lg: "none" }}
+                w={10}
+                h={10}
+                onClick={onOpen}
+                color="blue.900"
+                cursor="pointer"
+                mr="40px"
+              />
+              <Icon
+                as={GiHamburgerMenu}
+                display={{ base: "none", md: "none", lg: "block" }}
+                w={10}
+                h={10}
+                onClick={onClose}
+                color="blue.900"
+                cursor="pointer"
+                mr="40px"
+              />
+              <InputGroup w="60%">
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<AiOutlineSearch />}
+                />
+                <Input type="text" placeholder="Search" />
+                <InputRightElement
+                  cursor="pointer"
+                  children={<IconAdjustmentsHorizontal />}
+                />
+              </InputGroup>
+              <Spacer />
+              <Popover>
+                <PopoverTrigger>
+                  <Box>
+                    <Avatar bg="blue.900" size="sm" />
+                  </Box>
+                </PopoverTrigger>
+                <PopoverContent w="160px" mr="20px">
+                  <PopoverArrow />
+                  <PopoverBody>Are you sure</PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </Flex>
+          </Box>
+          <Box pt={{ base: "64px", md: "64px", lg: "80px" }}>{children}</Box>
+        </Box>
+      </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerContent
+          bg="blue.900"
+          display={{ base: "block", md: "block", lg: "none" }}
+        >
+          <Box
+            w="240px"
+            height="100vh"
+            boxShadow="md"
+            display={{ base: "none", lg: "block" }}
+          ></Box>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
