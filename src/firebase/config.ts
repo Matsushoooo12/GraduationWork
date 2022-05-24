b9a02fc2a15cbd757384d3e4ee0b5deb5a8166da
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-05-24 14:29:04 +0900

    firebase 認証機能初期化

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
@@ -14,3 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const auth = getAuth();
+
+export const googleProvider = new GoogleAuthProvider();
